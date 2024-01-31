38503c0c3af5a743e96a014ed83270fdd73d5db8
ramizqazi <ramizqazi03@gmail.com>
2024-01-31 11:18:14 +0500

    initialized react app

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {
+  ChakraProvider,
+  Box,
+  theme,
+} from '@chakra-ui/react';
+
+function App() {
+  return (
+    <ChakraProvider theme={theme}>
+      <Box textAlign="center" fontSize="xl">
+     
+      </Box>
+    </ChakraProvider>
+  );
+}
+
+export default App;
